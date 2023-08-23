f9e5675db4e661134950b7de6dc09fb36588d029
Luiz Augusto <augustodia97@gmail.com>
2023-08-23 15:51:44 -0300

    🔥 remove unnecessary integration with Google Maps

--- src/routes/routes.module.ts
+++ src/routes/routes.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { RoutesController } from './routes.controller';
 import { RoutesService } from './routes.service';
-import { MapsModule } from '../maps/maps.module';
 
 @Module({
-  imports: [MapsModule],
   controllers: [RoutesController],
   providers: [RoutesService],
 })
--- src/routes/routes.service.ts
+++ src/routes/routes.service.ts
@@ -1,32 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { PrismaService } from '../prisma/prisma/prisma.service';
-import { DirectionsService } from '../maps/directions/directions.service';
 
 @Injectable()
 export class RoutesService {
-  constructor(
-    private prismaService: PrismaService,
-    private directionsService: DirectionsService,
-  ) {}
+  constructor(private prismaService: PrismaService) {}
 
   async create(createRouteDto: CreateRouteDto) {
-    const { routes } = await this.directionsService.getDirections(
-      createRouteDto.source_id,
-      createRouteDto.destination_id,
-    );
-
-    const legs = routes[0].legs[0];
     return this.prismaService.route.create({
       data: {
         name: createRouteDto.name,
         source: {
-          lat: legs.start_location.lat,
-          lng: legs.start_location.lng,
+          lat: Math.random() * -100,
+          lng: Math.random() * 100,
         },
         destination: {
-          lat: legs.end_location.lat,
-          lng: legs.end_location.lng,
+          lat: Math.random() * -100,
+          lng: Math.random() * 100,
         },
       },
     });
